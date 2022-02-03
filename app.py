0a4fb602bc15bd562a0653b8d79929cdabe62f53
olegpelin <pelinoleg@gmail.com>
2022-02-03 22:33:12 +0200

    commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template, request, url_for, flash, redirect
+from flask import Flask, render_template, request, url_for, flash, redirect, render_template_string
 from werkzeug.exceptions import abort
 from datetime import datetime, timedelta
 import calendar
@@ -810,3 +810,72 @@ def edit_budget(id):
 
     return render_template('edit_budget.html', title='Edit Budget', gradient='text-gradient-blue',
                            categories=categories, budget=budget)
+
+
+@app.route('/test')
+def test():
+    return render_template_string('''
+<video id="video" width="640" height="480" autoplay style="background-color: grey"></video>
+<button id="send">Take & Send Photo</button>
+<canvas id="canvas" width="640" height="480" style="background-color: grey"></canvas>
+
+<script>
+
+// Elements for taking the snapshot
+var video = document.getElementById('video');
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+// Get access to the camera!
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        video.srcObject = stream;
+        video.play();
+    });
+}
+
+// Trigger photo take
+document.getElementById("send").addEventListener("click", function() {
+    context.drawImage(video, 0, 0, 640, 480); // copy frame from <video>
+    canvas.toBlob(upload, "image/jpeg");  // convert to file and execute function `upload`
+});
+
+function upload(file) {
+    // create form and append file
+    var formdata =  new FormData();
+    formdata.append("snap", file);
+
+    // create AJAX requests POST with file
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "{{ url_for('upload') }}", true);
+    xhr.onload = function() {
+        if(this.status = 200) {
+            console.log(this.response);
+        } else {
+            console.error(xhr);
+        }
+        alert(this.response);
+    };
+    xhr.send(formdata);
+}
+
+</script>
+''')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # fs = request.files['snap'] # it raise error when there is no `snap` in form
+        fs = request.files.get('snap')
+        if fs:
+            print('FileStorage:', fs)
+            print('filename:', fs.filename)
+            fs.save('image.jpg')
+            return 'Got Snap!'
+        else:
+            return 'You forgot Snap!'
+
+    return 'Hello World!'
